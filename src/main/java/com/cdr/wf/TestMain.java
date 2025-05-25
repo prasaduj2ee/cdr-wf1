80fdf23d268cdef5cad974ceae1d6464c038ce16
prasaduj2ee <prasadu.j2ee@gmail.com>
2025-05-25 07:29:45 +0530

    cdr

---
+++ src/main/java/com/cdr/wf/TestMain.java
@@ -0,0 +1,14 @@
+package com.cdr.wf;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TestMain {
+    public static void Tain(String[] args123) {
+        System.out.printf("Hello and welcome!");
+
+        String xyaz = "Hello";
+
+        for (int i = 1; i <= 5; i++) {System.out.println("i = " + i);
+        }
+    }
+}
